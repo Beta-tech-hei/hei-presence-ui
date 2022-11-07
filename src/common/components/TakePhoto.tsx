c975c0464c09be34940e7a99f45ece6d102fc803
Jonas4884 <hei.jonatana@gmail.com>
2022-11-07 12:34:37 +0300

    fix:code and folder review

--- src/common/components/TakePhoto.tsx
+++ src/common/components/TakePhoto.tsx
@@ -58,7 +58,6 @@ export const TakePhoto = () => {
                                                 videoConstraints={videoConstraints}
                                             />
                                         ) : null
-
                                 }
 
                             </div>
